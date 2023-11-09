562b7ea442701ea1dbc3e4a4bd7aa4740c5777a1
Freilyn Bernabe <freilynbpwork@gmail.com>
2023-11-09 11:57:39 -0400

    Added home screen and 404 route

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,14 @@ app.use(express.json());
 const PORT = 3000;
 
 app.get('/', (_req: Request, res: Response) => {
-	res.send('Hello, World! from express');
+	res.sendFile('client/index.html', { root: __dirname });
 });
 
 app.use('/api/notes', router);
+
+app.use((req, res) => {
+	res.status(404).send({ message: `route {{${req.path}}} not found` });
+});
 
 app.listen(PORT, () => {
 	console.log(`Server is running on PORT ${PORT}`);
